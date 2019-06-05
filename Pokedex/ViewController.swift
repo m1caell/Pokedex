5b4a12946289f66683d5cc3b43d60071bd33670e
Pablo Trindade <pablotrindade93@gmail.com>
2019-06-05 09:07:48 -0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Pablo Trindade on 05/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
