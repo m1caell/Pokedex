b0a2db5815d6261c177da28e48669677ec717c58
Pablo Trindade <pablotrindade93@gmail.com>
2019-06-15 10:12:29 -0300

    create first test

---
+++ PokedexTests/PokedexTests.swift
@@ -0,0 +1,37 @@
+//
+//  PokedexTests.swift
+//  PokedexTests
+//
+//  Created by Pablo Trindade on 15/06/19.
+//  Copyright © 2019 CWI Software. All rights reserved.
+//
+
+import XCTest
+@testable import Pokedex
+
+class PokedexTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testExample() {
+        let jsonURL = Bundle(for: PokedexTests.self).url(forResource: "pokemons", withExtension: "json")!
+        let data = try! Data(contentsOf: jsonURL)
+        
+        XCTAssertNoThrow(try RequestMaker.decoder.decode(PokemonResponse.self , from: data))
+    }
+
+}
